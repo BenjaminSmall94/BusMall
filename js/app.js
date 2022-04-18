546145ca9b5e3eb5298037234234b4e1390e1214
Benjamin Small <smallbenjamin94@gmail.com>
2022-04-18 15:14:25 -0700

    added generate randomIndices function

--- js/app.js
+++ js/app.js
@@ -3,9 +3,45 @@
 // ******* Global Variables *********
 
 let products = [];
+let randomIndices = [];
 let votingRounds = 25;
 
 // ******* DOM References ***********
+
+
+
+// ************ Main ****************
+
+fillProductArray;
+randomIndices = generateIndices(3);
+
+/*Events/Handlers
+Image Selection Listeners
+Button Listener
+
+Track Image Stats and create three more random photos
+*/
+
+// Functions
+
+function generateIndices(numOfPics) {
+  for(let i = 0; i < numOfPics; i++) {
+    let randomIndex = randomInRange(0, products.length - 1);
+    while(randomIndices.contains(randomIndex)) {
+      randomIndex = randomInRange(0, products.length - 1);
+    }
+    randomIndices.push(randomIndex);
+  }
+  return randomIndices;
+}
+
+function renderPics() {
+
+}
+
+function randomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function Product(name, fileExtension = '.jpg') {
   this.productName = name;
@@ -17,8 +53,8 @@ function Product(name, fileExtension = '.jpg') {
 }
 
 function fillProductArray() {
-  let productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-dock', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wineglass']
-  for(let i = 0; i < productNames.length, i++) {
+  let productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-dock', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wineglass'];
+  for(let i = 0; i < productNames.length; i++) {
     if(i !== 14) {
       new Product(productNames[i]);
     } else {
@@ -26,15 +62,3 @@ function fillProductArray() {
     }
   }
 }
-
-/*Events/Handlers
-    Image Selection Listeners
-    Button Listener
-
-    Track Image Stats and create three more random photos
-*/
-
-/*Functions
-    Randomly Generate Three Images
-    Display Randomly Selected Images
-*/
